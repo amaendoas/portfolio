3c33aa072d1d5262d7c93401ba77845203cb0193
Amanda Guerra <guerraamanda1@hotmail.com>
2022-08-25 18:17:41 -0300

    Atualização de currículo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -26,7 +26,7 @@ export function Home() {
               <img src={star11} alt="" className="star11"/>
                 <h1>HELLO WORLD!</h1>
                 <p> My name is <strong>Amanda Guerra</strong>, I'm a <u>web developer</u>  with main interest in <strong>front-end</strong> and <strong>UX/UI design</strong>.</p>
-                <a href="https://drive.google.com/uc?export=download&id=1bBKKjvVO9OkTxDS0FDCp0C7pj7uwwkmf">
+                <a href="https://drive.google.com/u/0/uc?id=1pYHW-EizifOGjIMWqN9kuFeL76mfsRlJ&export=download" target="_blank">
                 <Button icon={download} title="download resume"/>
                 </a>
             </C.PopUp>
