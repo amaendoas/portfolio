a20e2c46bfac3f4e3038cf432473971cfed47b12
Amanda Guerra <guerraamanda1@hotmail.com>
2023-07-07 14:32:57 -0300

    substituição do svg das entrelas na página stack

--- src/components/StackItem/index.jsx
+++ src/components/StackItem/index.jsx
@@ -1,6 +1,5 @@
 import * as C from "./styles"
 import { StarRating } from "../StarRating"
-import star from "../../assets/star-rate.svg"
 
 export function StackItem({icon: Icon, color, title, value}) {
   
--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,4 +1,3 @@
-import { MdStar } from "react-icons/md";
 import { Container } from "./styles";
 import starRate from "../../assets/star-rate.svg"
 
@@ -6,14 +5,9 @@ import starRate from "../../assets/star-rate.svg"
 export function StarRating({value = 0, ...rest}) {
   return (
     <Container>
-      {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1
+      {[...Array(value)].map(() => {
         return(
-        <MdStar
-        key={String(i)}
-        className="star"
-        color={ratingValue <= (value) ? "#E7C63C" : "#6F6B5F"}
-        />
+          <img src={starRate} alt="" className="star" />
         )
       }
       )}
